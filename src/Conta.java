96db2f4500132b4f085de94b372c58a62a258d71
Marcio de Andrade <marcio@requestdev.net>
2024-07-23 15:34:19 -0400

    estudos orientação a objetos

---
+++ src/Conta.java
@@ -0,0 +1,36 @@
+
+public abstract class Conta implements IConta {
+	
+	private static final int AGENCIA_PADRAO = 001;
+	
+	protected int agencia;
+	protected int numero;
+	private double saldo;
+	
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+}
